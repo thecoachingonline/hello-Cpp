e0add2ee9567988e8405907d4871e3c4e02a7cbb
Y.Nanthachai <thecoachingonline@gmail.com>
2023-03-30 12:11:22 +0700

    Classes

--- hello.cpp
+++ hello.cpp
@@ -6,3 +6,26 @@ int main() {
     std::cout << "Hello, world!" << std::endl;
     return (0);
 }
+
+// Classes
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum cow_purpose { dairy, meat, hide, pet };
+
+class cow{
+private:
+    string name;
+    int age;
+    unsigned char purpose;
+};
+
+int main(){
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Betsy";
+    my_cow.purpose = dairy;
+    return(0);
+};
